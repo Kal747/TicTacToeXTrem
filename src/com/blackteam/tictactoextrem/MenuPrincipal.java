59f516a4b235b57ab4ada070d2d8c940d101244c
Kal747 (Jeremy) <kal747@hotmail.fr>
2013-01-04 01:06:59 +0100

    Ajout menu principal

---
+++ src/com/blackteam/tictactoextrem/MenuPrincipal.java
@@ -0,0 +1,40 @@
+package com.blackteam.tictactoextrem;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MenuPrincipal extends Activity {
+	
+	private Handler _mHandler = new Handler();
+	
+	private Runnable _stop = new Runnable() {
+		public void run() {
+			System.exit(RESULT_OK);
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_principal);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_menu_principal, menu);
+		return true;
+	}
+	
+	public void onClickButtonJouerEnLocal(View view) {
+	    System.out.println("Boutton jouer en local");
+	}
+	
+	public void onClickButtonQuitter(View view) {
+		_mHandler.postDelayed(_stop, 0);
+	}
+
+}
