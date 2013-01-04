47a4ad3f6fdadd8db1319784e9dbd74adfcfeb86
bocoum <bocoum@localhost.localdomain>
2013-01-04 02:01:58 +0100

    Ajout du splash screen

--- src/com/blackteam/tictactoextrem/SplashActivity.java
+++ src/com/blackteam/tictactoextrem/SplashActivity.java
@@ -1,33 +1,22 @@
 package com.blackteam.tictactoextrem;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class SplashActivity extends Activity {
 
-	private Handler mHandler ;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-		mHandler = new Handler();
-		mHandler.postDelayed(mUpdateTimeTask, 2000);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_splash, menu);
 		return true;
 	}
-	
-	private Runnable mUpdateTimeTask = new Runnable() {
-	  public void run() {
-	     setContentView(R.layout.activity_menu_principal );
-	  }
-	};
 
 }
