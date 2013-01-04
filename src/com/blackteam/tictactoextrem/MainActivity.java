9036787c48ad5cdcad61f0b1d91cb0f7ab67456c
bocoum <bocoum@localhost.localdomain>
2013-01-04 02:56:40 +0100

    Splash avec image dans la main activity

--- src/com/blackteam/tictactoextrem/MainActivity.java
+++ src/com/blackteam/tictactoextrem/MainActivity.java
@@ -3,18 +3,20 @@ package com.blackteam.tictactoextrem;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
 	 
-	
+	private Handler mHandler = new Handler();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		mHandler = new Handler();
+		mHandler.postDelayed(mUpdateTimeTask, 5000);
 	}
 
 	@Override
@@ -24,6 +26,11 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	
-
+	private Runnable mUpdateTimeTask = new Runnable() {
+		  public void run() {
+			 Intent intent = new Intent(MainActivity.this, MenuPrincipal.class);
+			 startActivity(intent);
+		     finish();
+		  }
+	};
 }
