0d6f12b99fd4bc4039d1ec3096ff521225d08d1b
bocoum <bocoum@localhost.localdomain>
2013-01-05 18:47:16 +0100

    ajout de message gagnat

--- src/com/blackteam/tictactoextrem/JeuActivity.java
+++ src/com/blackteam/tictactoextrem/JeuActivity.java
@@ -1,9 +1,10 @@
 package com.blackteam.tictactoextrem;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -25,14 +26,14 @@ public class JeuActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_jeu, menu);
 		return true;
 	}
-	//	onClickButtonMenuPrincipal
+	 
 
 	public void onClickButtonMenuPrincipal(View view) {
 		System.out.println("onClickButtonMenuPrincipal");
 		Intent intent = new Intent(JeuActivity.this, MenuPrincipal.class);
 		startActivity(intent);
 		finish();
-		
+
 	}
 
 	public void onClickButtonSquare(View view) {
@@ -43,7 +44,7 @@ public class JeuActivity extends Activity {
 		// Si incorrect ou jeu fini : 0
 		int jouerId = MainActivity.game.put(x, y);
 		if(jouerId != 0) {
-			 
+
 			if(jouerId==1)
 				((Button)view).setBackgroundResource(R.drawable.  croixfini);
 			else 
@@ -52,12 +53,42 @@ public class JeuActivity extends Activity {
 			MainActivity.vi.vibrate(500);
 		}
 		System.out.println(MainActivity.game);
-	}
-	
-	 
-	 
-	   
-	     
-	
 
+		if(    MainActivity.game.winner()==1 ||  MainActivity.game.winner()==2 ){
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			// set title
+			alertDialogBuilder.setTitle("Your Title");
+			// set dialog message
+			alertDialogBuilder
+			.setMessage("Joueur "+MainActivity.game.winner() + " a gagné")
+			.setCancelable(false)
+			.setPositiveButton("Menu principal",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					Intent intent = new Intent(JeuActivity.this, MenuPrincipal.class);
+					startActivity(intent);
+					finish();
+				}
+			})
+			.setNegativeButton("Rejouer",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					MainActivity.game.launch();
+					Intent intent = new Intent(JeuActivity.this, JeuActivity.class);
+					startActivity(intent);
+					finish();
+				}
+			});
+			// create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			// show it
+			alertDialog.show();
+		}
+	}
 }
+
+
+
+
+
+
+
+
